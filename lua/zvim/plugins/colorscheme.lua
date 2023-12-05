be1f309f7a6c6cde93332942e1a8636e3f869e3a
Zain Dewsi <zain.dewsi@gmail.com>
2023-12-05 09:30:35 -0500

    autopairs

--- lua/zvim/plugins/colorscheme.lua
+++ lua/zvim/plugins/colorscheme.lua
@@ -2,7 +2,7 @@ return {{
     "folke/tokyonight.nvim",
     config = function()
         require("tokyonight").setup({
-            transparent = true
+            transparent = false
         })
         vim.cmd.colorscheme 'tokyonight-storm'
     end
